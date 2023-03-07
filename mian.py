870dcc51a06f3c133226664a93991d31ddd48a3c
Baranova Ekaterina <recrane@rambler.ru>
2023-03-07 22:29:34 +1000

    chamge file

--- mian.py
+++ mian.py
@@ -3,6 +3,7 @@ import requests
 from fake_headers import Headers
 from bs4 import BeautifulSoup
 
+parsed_list = []
 
 HOST = 'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
 #ARTICLES = f'{HOST}/ru/all/'
@@ -17,12 +18,40 @@ hh_main = response.text
 soup = BeautifulSoup(hh_main, features='lxml')
 
 vacations = soup.find_all('div', class_='serp-item')
-#vacations = vacation_list.find('vacation')
-print(vacations)
-#print(soup)
+#salary = soup.find_all(attrs={'data-qa' : 'vacancy-serp__vacancy-compensation'})
 
-company = soup.find('div', class_='serp-item__title')
-print(company)
+#vacations = vacation_list.find('vacation')
+#print(salary)
+
+
+for vac in vacations:
+    hh_vac = vac.find('a')
+    vac_link = hh_vac['href']
+    #print(vac_link)
+    company_name = vac.find('div', {'class': 'bloko-text'}).find('a').text
+    #print(company_name)
+    city = vac.find(attrs={'data-qa' : 'vacancy-serp__vacancy-address'}).text
+    #print(city)
+    salary = vac.find(attrs={'data-qa' : 'vacancy-serp__vacancy-compensation'})
+    
+    if salary == None:
+        salary_num = ''
+    else:
+        salary_num = salary.text
+    #print(salary_num)
+
+    item = {
+        'link': vac_link,
+        'salary': salary_num,
+        'company': company_name,
+        'city': city
+    }
+    parsed_list.append(item)
+
+print(parsed_list)
+    
+
+ 
     
     
 
